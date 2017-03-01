f4e8d627763085ea26d12df121fcd6ace06324bf
haroldasai <koichiroasai@gmail.com>
2017-03-01 05:03:32 -0800

    Update Main.java

--- 2_x_Linked_List/Java/Main.java
+++ 2_x_Linked_List/Java/Main.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package pkg2_x_linked_list;
+//package pkg2_x_linked_list;
 import java.util.*;
 /**
  *
